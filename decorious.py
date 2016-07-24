6e5b766b1c9c3e5fdf66e81cbab0cf4ec68f5701
Attilio <parrotchaffinch@gmail.com>
2016-07-24 09:54:18 +0200

    Base implementation.

--- decorious.py
+++ decorious.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
 
+import PIL.Image, PIL.ImageFilter
+
 
 class IImage:
-  pass;
+  def getImgObj(self):
+    raise NotImplementedError("cannot call method of IImage.getImgArr on the interface")
 
 class BaseImage(IImage):
-  pass;
+  def __init__(self, imgObj):
+    self.imgObj = imgObj
+
+  def getImgObj(self):
+    return self.imgObj
+
 
 class Filter(IImage):
+  def getImgObj(self):
+    raise NotImplementedError("cannot call method of Filter.getImgArr on the base class")
   def __init__(self, img):
     self.img = img
 
@@ -16,3 +26,7 @@ class BlurFilter(Filter):
   def __init__(self, img, radius):
     Filter.__init__(self, img)
     self.radius = radius
+  def getImgObj(self):
+    imageIternal = self.img.getImgObj()
+    return imageIternal.filter(PIL.ImageFilter.GaussianBlur(self.radius))
+    
